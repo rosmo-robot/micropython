8843b3747eefb03d10160ecc6317e92107f49bad
samuk <sam@bristolwireless.net>
2024-12-27 22:57:43 +0000

    Update RosMo_WebInterface.py
    Made a bit easier on mobile devices. Changed font from purple to grey

--- RosMo_WebInterface.py
+++ RosMo_WebInterface.py
@@ -8,12 +8,12 @@ speed = 0.5
 
 def webpage(dist):
     global interface
-    print ("INTFC: " + interface)
+    print("INTFC: " + interface)
     if interface == "phone":
         return phonepage(dist)
     CSS = getCSS()
     CONTROL = controlTable(dist)
-    #Template HTML
+    # Template HTML
     html = f"""
             <!DOCTYPE html>
             <html>
@@ -24,7 +24,7 @@ def webpage(dist):
             </style>
             </head>
             <body>
-            <div align = "center">
+            <div align="center">
             <h1>RosMo (PC interface)</h1>
             {CONTROL}  
             </div>
@@ -36,7 +36,7 @@ def webpage(dist):
 def phonepage(dist):
     CSS = getCSS()
     CONTROL = controlTable(dist)
-    #Template HTML
+    # Template HTML
     html = f"""
             <!DOCTYPE html>
             <html>
@@ -47,7 +47,7 @@ def phonepage(dist):
             </style>
             </head>
             <body>
-            <div align = "center">
+            <div align="center">
             <h1>RosMo (Phone interface)</h1>
             {CONTROL}
             </div>
@@ -58,7 +58,7 @@ def phonepage(dist):
 
 def BLANKpage():
     CSS = getCSS()
-    #Template HTML
+    # Template HTML
     html = f"""
             <!DOCTYPE html>
             <html>
@@ -68,7 +68,7 @@ def BLANKpage():
             </style>
             </head>
             <body>
-            <div align = "center">
+            <div align="center">
             <h1>RosMo</h1>
             PLEASE WAIT FOR CURRENT COMMAND TO COMPLETE
             </div>
@@ -77,15 +77,16 @@ def BLANKpage():
             """
     return str(html)
 
-
 def getCSS():
     css = """
      body {
             color:#eae0c2;
             background-color: #1c1b18;
             font-family:Arial;
-            font-size:14px;
+            font-size:28px;
             font-weight:bold;
+            margin: 0;
+            padding: 0;
         }
 
         input {
@@ -98,12 +99,14 @@ def getCSS():
             cursor:pointer;
             color:#505739;
             font-family:Arial;
-            font-size:14px;
+            font-size:16px;
             font-weight:bold;
-            padding:12px 16px;
+            padding:40px 20px; /* Doubled padding for twice the depth */
             text-decoration:none;
             text-shadow:0px 1px 0px #ffffff;
-            width: 146px
+            width: 100%;
+            box-sizing: border-box;
+            margin: 10px 0; /* Added margin for spacing */
         }
         input:hover {
             background:linear-gradient(to bottom, #ccc2a6 5%, #eae0c2 100%);
@@ -115,18 +118,22 @@ def getCSS():
         }     
  
         .direction {
-            color:#FF00FF;
+            color:#444444; /* Changed to dark grey */
             font-size:20px;
-            width: 50px;
         }  
  
         .speed {
-            color:#FF00FF;
+            color:#444444; /* Changed to dark grey */
             font-size:20px;
-            width: 120px;
         }
         .control {
-            color:#FF00FF;
+            color:#444444; /* Changed to dark grey */
+        }
+        table {
+            width: 100%;
+        }
+        td {
+            padding: 5px;
         }
         """
     return str(css)
@@ -140,44 +147,61 @@ def controlTable(dist):
         speedstr = "fast"
     else:
         speedstr = "medium"
-    
-    
-    
+
     if interface == "phone":
         link = "<form action='./PC'><input type='submit' value='PC Interface' /></form>"
     else:
         link = "<form action='./PH'><input type='submit' value='Phone Interface' /></form>"
-        
-    
+
     control = f"""
-
-
       <h3 class='control'>Control</h3>
-      <table width = "70%"><tr><td width = "33%" align = "center">            
-        {link}        
-        </td><td width = "33%" align = "center">
-        <table><tr><td><form action='./CL'><input type='submit' value='&nwarhk;' class='direction' /></form></td>
-          <td><form action='./FW'><input type='submit' value='&uarr;' class='direction' /></form></td>
-          <td><form action='./CR'><input type='submit' value='&nearhk;' class='direction' /></form></td>
-          <td><form action='./SS'><input type='submit' value='slow' class='speed' /></form></td>
-          </tr><tr>
-          <td><form action='./L'><input type='submit' value='&olarr;' class='direction' /></form></td>
-          <td><form action='./STP'><input type='submit' value='&excl;' class='direction' /></form></td>
-          <td><form action='./R'><input type='submit' value='&orarr;' class='direction' /></form></td>
-          <td><form action='./SM'><input type='submit' value='medium' class='speed' /></form></td>
-          </tr><tr>
-          <td>&nbsp;</td>
-          <td><form action='./BK'><input type='submit' value='&darr;' class='direction' /></form></td>
-          <td>&nbsp;</td>
-          <td><form action='./SF'><input type='submit' value='fast' class='speed' /></form></td>
-          </tr>
-        </table>
-      </td>
-      <td align = 'center'>Distance: {dist} cm (not implemented)<br /><br />
-      Speed: {speedstr}<br /><br /><form action='./UPD'><input type='submit' value='Refresh' /></form>
-       </td>
+      <table>
+        <tr>
+          <td>
+            <form action='./CL'><input type='submit' value='&nwarhk;' class='direction' /></form>
+          </td>
+          <td>
+            <form action='./FW'><input type='submit' value='&uarr;' class='direction' /></form>
+          </td>
+          <td>
+            <form action='./CR'><input type='submit' value='&nearhk;' class='direction' /></form>
+          </td>
         </tr>
-    </table>
+        <tr>
+          <td>
+            <form action='./L'><input type='submit' value='&olarr;' class='direction' /></form>
+          </td>
+          <td>
+            <form action='./STP'><input type='submit' value='&excl;' class='direction' /></form>
+          </td>
+          <td>
+            <form action='./R'><input type='submit' value='&orarr;' class='direction' /></form>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <form action='./BK'><input type='submit' value='&darr;' class='direction' /></form>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <form action='./SS'><input type='submit' value='slow' class='speed' /></form>
+          </td>
+          <td>
+            <form action='./SM'><input type='submit' value='medium' class='speed' /></form>
+          </td>
+          <td>
+            <form action='./SF'><input type='submit' value='fast' class='speed' /></form>
+          </td>
+        </tr>
+      </table>
+      <div align="center" style="margin-top: 20px;">
+        Distance: {dist} cm (not implemented)<br />
+        Speed: {speedstr}<br /><br />
+        <form action='./UPD'><input type='submit' value='Refresh' /></form>
+        <br /><br />
+        {link}
+      </div>
     """
     return str(control)
 
